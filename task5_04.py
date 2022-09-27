bf8290367d5c6dae0e8b52e859b2ccbc39e7dd40
Nikita Kozlov <kozlov.nu@gmail.com>
2022-09-27 12:57:28 +0300

    Add task5_04

---
+++ task5_04.py
@@ -0,0 +1,15 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+string = list('aaaabbbcc')
+print(string)
+
+count = 1
+
+for i in range(len(string)-1):
+    if i <= len(string):
+        if string[i] == string[i + 1]:
+            count += 1
+        else:
+            a = string[i]
+            print(count, string[i])
+            count = 1
+print(count, string[i])
